e272020916e41a8f75e9e1820ac3c4b76d0a58fe
Aaron Meurer <asmeurer@gmail.com>
2022-09-26 16:28:08 -0600

    Add some basic aliases, and some basic documentation

---
+++ numpy_array_api_compat/_aliases.py
@@ -0,0 +1,20 @@
+"""
+These are functions that are just aliases of existing functions in NumPy.
+"""
+
+from numpy import (arccos, arccosh, arcsin, arcsinh, arctan, arctan2, arctanh,
+                   left_shift, invert, right_shift, bool_, concatenate, power)
+
+acos = arccos
+acosh = arccosh
+asin = arcsin
+asinh = arcsinh
+atan = arctan
+atan2 = arctan2
+atanh = arctanh
+bitwise_left_shift = left_shift
+bitwise_invert = invert
+bitwise_right_shift = right_shift
+bool = bool_
+concat = concatenate
+pow = power
