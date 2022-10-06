fbaf586d796799eeb31752c763156640226d6d4f
Aaron Meurer <asmeurer@gmail.com>
2022-10-05 18:46:25 -0600

    Add a comment with known differences from the spec to the docstring

--- numpy_array_api_compat/__init__.py
+++ numpy_array_api_compat/__init__.py
@@ -16,6 +16,16 @@ Library authors using the Array API may wish to test against numpy.array_api
 to ensure they are not using functionality outside of the standard, but prefer
 this implementation for the default when working with NumPy arrays.
 
+Known differences from the Array API spec:
+
+- The array methods __array_namespace__, device, to_device, and mT are not
+  defined. This reuses np.ndarray and we don't want to monkeypatch or wrap it.
+
+- NumPy value-based casting for scalars will be in effect unless explicitly
+  disabled with the environment variable NPY_PROMOTION_STATE=weak or
+  np._set_promotion_state('weak') (requires NumPy 1.24 or newer, see NEP 50
+  and https://github.com/numpy/numpy/issues/22341)
+
 """
 
 from numpy import *
