53557d4bcc18007f309ddb77b06b12b71a00a0b1
Aaron Meurer <asmeurer@gmail.com>
2022-11-07 16:15:52 -0700

    Fix an import in __init__.py so the package can be vendored

--- numpy_array_api_compat/__init__.py
+++ numpy_array_api_compat/__init__.py
@@ -46,12 +46,14 @@ from numpy import *
 # These imports may overwrite names from the import * above.
 from ._aliases import *
 
-# Don't know why, but we have to do this to import linalg. If we instead do
+# Don't know why, but we have to do an absolute import to import linalg. If we
+# instead do
 #
 # from . import linalg
 #
-# It doesn't overwrite np.linalg from above.
-import numpy_array_api_compat.linalg
+# It doesn't overwrite np.linalg from above. The import is generated
+# dynamically so that the library can be vendored.
+__import__(__package__ + '.linalg')
 
 from .linalg import matrix_transpose, vecdot
 
