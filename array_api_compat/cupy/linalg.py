5775b11dd011dd835569699a812580d842a46d47
Aaron Meurer <asmeurer@gmail.com>
2022-11-28 22:42:48 -0700

    Rename common.linalg to common._linalg

--- array_api_compat/cupy/linalg.py
+++ array_api_compat/cupy/linalg.py
@@ -8,28 +8,29 @@ del _n['__builtins__']
 linalg_all = list(_n)
 del _n
 
-from ..common import linalg
+from ..common import _linalg
 from .._internal import get_xp
 
 import cupy as cp
 
-cross = get_xp(cp)(linalg.cross)
-diagonal = get_xp(cp)(linalg.diagonal)
-matmul = get_xp(cp)(linalg.matmul)
-cholesky = get_xp(cp)(linalg.cholesky)
-matrix_rank = get_xp(cp)(linalg.matrix_rank)
-pinv = get_xp(cp)(linalg.pinv)
-matrix_norm = get_xp(cp)(linalg.matrix_norm)
-matrix_transpose = get_xp(cp)(linalg.matrix_transpose)
-outer = get_xp(cp)(linalg.outer)
-svdvals = get_xp(cp)(linalg.svdvals)
-tensordot = get_xp(cp)(linalg.tensordot)
-trace = get_xp(cp)(linalg.trace)
-vecdot = get_xp(cp)(linalg.vecdot)
-vector_norm = get_xp(cp)(linalg.vector_norm)
+cross = get_xp(cp)(_linalg.cross)
+diagonal = get_xp(cp)(_linalg.diagonal)
+matmul = get_xp(cp)(_linalg.matmul)
+cholesky = get_xp(cp)(_linalg.cholesky)
+matrix_rank = get_xp(cp)(_linalg.matrix_rank)
+pinv = get_xp(cp)(_linalg.pinv)
+matrix_norm = get_xp(cp)(_linalg.matrix_norm)
+matrix_transpose = get_xp(cp)(_linalg.matrix_transpose)
+outer = get_xp(cp)(_linalg.outer)
+svdvals = get_xp(cp)(_linalg.svdvals)
+tensordot = get_xp(cp)(_linalg.tensordot)
+trace = get_xp(cp)(_linalg.trace)
+vecdot = get_xp(cp)(_linalg.vecdot)
+vector_norm = get_xp(cp)(_linalg.vector_norm)
 
-__all__ = linalg_all + linalg.__all__
+__all__ = linalg_all + _linalg.__all__
 
 del get_xp
 del cp
 del linalg_all
+del _linalg
--- array_api_compat/numpy/linalg.py
+++ array_api_compat/numpy/linalg.py
@@ -1,28 +1,29 @@
 from numpy.linalg import *
 from numpy.linalg import __all__ as linalg_all
 
-from ..common import linalg
+from ..common import _linalg
 from .._internal import get_xp
 
 import numpy as np
 
-cross = get_xp(np)(linalg.cross)
-diagonal = get_xp(np)(linalg.diagonal)
-matmul = get_xp(np)(linalg.matmul)
-cholesky = get_xp(np)(linalg.cholesky)
-matrix_rank = get_xp(np)(linalg.matrix_rank)
-pinv = get_xp(np)(linalg.pinv)
-matrix_norm = get_xp(np)(linalg.matrix_norm)
-matrix_transpose = get_xp(np)(linalg.matrix_transpose)
-outer = get_xp(np)(linalg.outer)
-svdvals = get_xp(np)(linalg.svdvals)
-tensordot = get_xp(np)(linalg.tensordot)
-trace = get_xp(np)(linalg.trace)
-vecdot = get_xp(np)(linalg.vecdot)
-vector_norm = get_xp(np)(linalg.vector_norm)
+cross = get_xp(np)(_linalg.cross)
+diagonal = get_xp(np)(_linalg.diagonal)
+matmul = get_xp(np)(_linalg.matmul)
+cholesky = get_xp(np)(_linalg.cholesky)
+matrix_rank = get_xp(np)(_linalg.matrix_rank)
+pinv = get_xp(np)(_linalg.pinv)
+matrix_norm = get_xp(np)(_linalg.matrix_norm)
+matrix_transpose = get_xp(np)(_linalg.matrix_transpose)
+outer = get_xp(np)(_linalg.outer)
+svdvals = get_xp(np)(_linalg.svdvals)
+tensordot = get_xp(np)(_linalg.tensordot)
+trace = get_xp(np)(_linalg.trace)
+vecdot = get_xp(np)(_linalg.vecdot)
+vector_norm = get_xp(np)(_linalg.vector_norm)
 
-__all__ = linalg_all + linalg.__all__
+__all__ = linalg_all + _linalg.__all__
 
 del get_xp
 del np
 del linalg_all
+del _linalg
