82365ebf22c85076094a9d66516c423c36f232a7
Aaron Meurer <asmeurer@gmail.com>
2022-11-30 17:36:56 -0700

    Add a test that vendoring works

---
+++ tests/test_vendoring.py
@@ -0,0 +1,15 @@
+from pytest import skip
+
+def test_vendoring_numpy():
+    from vendor_test import uses_numpy
+    uses_numpy._test_numpy()
+
+
+def test_vendoring_cupy():
+    try:
+        import cupy
+    except ImportError:
+        skip("CuPy is not installed")
+
+    from vendor_test import uses_cupy
+    uses_cupy._test_cupy()
