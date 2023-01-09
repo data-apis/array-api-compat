c484dbff2acc576a4362c95a06afd91507cbcbb2
Aaron Meurer <asmeurer@gmail.com>
2023-01-09 15:31:42 -0700

    Add max and min wrappers for torch

--- array_api_compat/torch/_aliases.py
+++ array_api_compat/torch/_aliases.py
@@ -11,6 +11,20 @@ array = torch.Tensor
 # Basic renames
 permute_dims = torch.permute
 
+# These wrappers are mostly based on the fact that pytorch uses 'dim' instead
+# of 'axis'.
+def max(x: array, /, *, axis: Optional[Union[int, Tuple[int, ...]]] = None, keepdims: bool = False) -> array:
+    # https://github.com/pytorch/pytorch/issues/29137
+    if axis == ():
+        return x
+    return torch.amax(x, axis, keepdims=keepdims)
+
+def min(x: array, /, *, axis: Optional[Union[int, Tuple[int, ...]]] = None, keepdims: bool = False) -> array:
+    # https://github.com/pytorch/pytorch/issues/29137
+    if axis == ():
+        return x
+    return torch.amin(x, axis, keepdims=keepdims)
+
 def expand_dims(x: array, /, *, axis: int = 0) -> array:
     return torch.unsqueeze(x, axis)
 
@@ -25,4 +39,4 @@ def full(shape: Union[int, Tuple[int, ...]],
 
     return torch.full(shape, fill_value, dtype=dtype, device=device, **kwargs)
 
-__all__ = ['permute_dims', 'expand_dims', 'full']
+__all__ = ['permute_dims', 'max', 'min', 'expand_dims', 'full']
