fa772de34fc906ca650e7e8bdeec2d192c9dc5b5
Aaron Meurer <asmeurer@gmail.com>
2023-03-13 18:25:36 -0600

    Fix test skipping for cupy

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -3,6 +3,6 @@ from importlib import import_module
 import pytest
 
 def import_(library):
-    if library == 'cupy':
+    if 'cupy' in library:
         return pytest.importorskip(library)
     return import_module(library)
