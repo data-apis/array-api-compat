6c338cabc082b32b2513b0d2b12691fe1af61801
Aaron Meurer <asmeurer@gmail.com>
2024-02-08 13:26:46 -0700

    Skip JAX tests in Python 3.8

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -1,11 +1,15 @@
 from importlib import import_module
 
+import sys
+
 import pytest
 
 
 def import_(library, wrapper=False):
     if library == 'cupy':
         return pytest.importorskip(library)
+    if 'jax' in library and sys.version_info <= (3, 8):
+        pytest.skip('JAX array API support does not support Python 3.8')
 
     if wrapper:
         if 'jax' in library:
