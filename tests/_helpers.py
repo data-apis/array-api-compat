4018fe43d0ead5ed83fbcf07580197801bc35f1e
Aaron Meurer <asmeurer@gmail.com>
2024-03-05 18:15:52 -0700

    Fix import_('cupy', wrapper=True) tests helper

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -6,8 +6,6 @@ import pytest
 
 
 def import_(library, wrapper=False):
-    if library == 'cupy':
-        return pytest.importorskip(library)
     if 'jax' in library and sys.version_info < (3, 9):
         pytest.skip('JAX array API support does not support Python 3.8')
 
@@ -16,5 +14,7 @@ def import_(library, wrapper=False):
             library = 'jax.experimental.array_api'
         else:
             library = 'array_api_compat.' + library
+    elif library == 'cupy':
+        return pytest.importorskip(library)
 
     return import_module(library)
