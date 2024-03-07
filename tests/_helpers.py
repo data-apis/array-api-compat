9e613cce3197f53f5f0bd7a03d676c4218521923
Aaron Meurer <asmeurer@gmail.com>
2024-03-07 14:28:09 -0700

    Fix cupy skipping in the tests

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -9,12 +9,12 @@ def import_(library, wrapper=False):
     if 'jax' in library and sys.version_info < (3, 9):
         pytest.skip('JAX array API support does not support Python 3.8')
 
+    if library == 'cupy':
+        pytest.importorskip(library)
     if wrapper:
         if 'jax' in library:
             library = 'jax.experimental.array_api'
         else:
             library = 'array_api_compat.' + library
-    elif library == 'cupy':
-        return pytest.importorskip(library)
 
     return import_module(library)
