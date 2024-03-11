b0ec8a603d1ca015d68d7494dacb48a2b2912017
Isaac Virshup <ivirshup@gmail.com>
2024-03-11 13:12:48 +0000

    test case for asarray

--- tests/test_common.py
+++ tests/test_common.py
@@ -42,6 +42,17 @@ def test_device(library):
     x2 = to_device(x, dev)
     assert device(x) == device(x2)
 
+@pytest.mark.parametrize("target_library,func", is_functions.items())
+@pytest.mark.parametrize("source_library", is_functions.keys())
+def test_asarray(source_library, target_library, func):
+    src_lib = import_(source_library, wrapper=True)
+    tgt_lib = import_(target_library, wrapper=True)
+    is_tgt_type = globals()[func]
+
+    a = src_lib.asarray([1, 2, 3])
+    b = tgt_lib.asarray(a)
+
+    assert is_tgt_type(b), f"Expected {b} to be a {tgt_lib.ndarray}, but was {type(b)}"
 
 @pytest.mark.parametrize("library", ["cupy", "numpy", "torch", "dask.array"])
 def test_to_device_host(library):
