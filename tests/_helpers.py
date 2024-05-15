efbd400f4f3a60a116ea5625d0ec1c2b9aa7b4d1
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2024-05-15 10:20:22 +0200

    Revert "Only test `sparse` when it can actually be imported."
    This reverts commit 44681b6b95c36f2428f2c0fc0ce4f1bd707ff1f8.

--- tests/_helpers.py
+++ tests/_helpers.py
@@ -14,9 +14,7 @@ if sys.version_info >= (3, 10):
     all_libraries.append('sparse')
 
 def import_(library, wrapper=False):
-    # CuPy requires a GPU
-    # `sparse` has a dependency conflict with NumPy 1.21
-    if library in {'cupy', 'sparse'}:
+    if library == 'cupy':
         pytest.importorskip(library)
     if wrapper:
         if 'jax' in library:
