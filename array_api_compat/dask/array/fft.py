1856ec72c8ec140e7bd70d75facaf5e2c12fe36d
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-09-28 14:32:42 -0400

    add wrappers for only fftfreq

--- array_api_compat/dask/array/fft.py
+++ array_api_compat/dask/array/fft.py
@@ -6,20 +6,20 @@ from ..._internal import get_xp
 
 import dask.array as da
 
-fft = get_xp(da)(_fft.fft)
-ifft = get_xp(da)(_fft.ifft)
-fftn = get_xp(da)(_fft.fftn)
-ifftn = get_xp(da)(_fft.ifftn)
-rfft = get_xp(da)(_fft.rfft)
-irfft = get_xp(da)(_fft.irfft)
-rfftn = get_xp(da)(_fft.rfftn)
-irfftn = get_xp(da)(_fft.irfftn)
-hfft = get_xp(da)(_fft.hfft)
-ihfft = get_xp(da)(_fft.ihfft)
+# fft = get_xp(da)(_fft.fft)
+# ifft = get_xp(da)(_fft.ifft)
+# fftn = get_xp(da)(_fft.fftn)
+# ifftn = get_xp(da)(_fft.ifftn)
+# rfft = get_xp(da)(_fft.rfft)
+# irfft = get_xp(da)(_fft.irfft)
+# rfftn = get_xp(da)(_fft.rfftn)
+# irfftn = get_xp(da)(_fft.irfftn)
+# hfft = get_xp(da)(_fft.hfft)
+# ihfft = get_xp(da)(_fft.ihfft)
 fftfreq = get_xp(da)(_fft.fftfreq)
 rfftfreq = get_xp(da)(_fft.rfftfreq)
-fftshift = get_xp(da)(_fft.fftshift)
-ifftshift = get_xp(da)(_fft.ifftshift)
+# fftshift = get_xp(da)(_fft.fftshift)
+# ifftshift = get_xp(da)(_fft.ifftshift)
 
 __all__ = fft_all + _fft.__all__
 
