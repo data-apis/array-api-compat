8d888d5104b145708c376439c7e12478e701e75a
crusaderky <crusaderky@gmail.com>
2025-02-27 15:40:58 +0000

    Re-add py.typed

--- setup.py
+++ setup.py
@@ -33,5 +33,8 @@ setup(
         "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    package_data={
+        "array_api_compat": ["py.typed"],
+    },
 )
