9643256ae1b7fd7af65b3255c992ce760dc6a4b5
jorenham <jhammudoglu@gmail.com>
2025-03-22 22:31:55 +0100

    TYP: fix typing errors in `numpy._typing`

--- array_api_compat/numpy/_typing.py
+++ array_api_compat/numpy/_typing.py
@@ -3,29 +3,22 @@ from __future__ import annotations
 __all__ = ["Array", "DType", "Device"]
 _all_ignore = ["np"]
 
-from typing import Literal, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Literal, TypeAlias
 
 import numpy as np
-from numpy import ndarray as Array
 
-Device = Literal["cpu"]
+Device: TypeAlias = Literal["cpu"]
 if TYPE_CHECKING:
     # NumPy 1.x on Python 3.10 fails to parse np.dtype[]
-    DType = np.dtype[
-        np.intp
-        | np.int8
-        | np.int16
-        | np.int32
-        | np.int64
-        | np.uint8
-        | np.uint16
-        | np.uint32
-        | np.uint64
+    DType: TypeAlias = np.dtype[
+        np.bool_
+        | np.integer[Any]
         | np.float32
         | np.float64
         | np.complex64
         | np.complex128
-        | np.bool
     ]
+    Array: TypeAlias = np.ndarray[Any, DType]
 else:
     DType = np.dtype
+    Array = np.ndarray
