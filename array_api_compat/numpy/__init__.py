dc79e3f1731b966294142067416e9c76cf5bb607
jorenham <jhammudoglu@gmail.com>
2025-03-22 22:26:36 +0100

    TYP: fix/ignore typing errors in `numpy.__init__`

--- array_api_compat/numpy/__init__.py
+++ array_api_compat/numpy/__init__.py
@@ -1,10 +1,16 @@
-from numpy import * # noqa: F403
+# ruff: noqa: PLC0414
+from typing import Final
+
+from numpy import *  # noqa: F403  # pyright: ignore[reportWildcardImportFromLibrary]
 
 # from numpy import * doesn't overwrite these builtin names
-from numpy import abs, max, min, round # noqa: F401
+from numpy import abs as abs
+from numpy import max as max
+from numpy import min as min
+from numpy import round as round
 
 # These imports may overwrite names from the import * above.
-from ._aliases import * # noqa: F403
+from ._aliases import *  # noqa: F403
 
 # Don't know why, but we have to do an absolute import to import linalg. If we
 # instead do
@@ -13,9 +19,17 @@ from ._aliases import * # noqa: F403
 #
 # It doesn't overwrite np.linalg from above. The import is generated
 # dynamically so that the library can be vendored.
-__import__(__package__ + '.linalg')
-__import__(__package__ + '.fft')
+__import__(__package__ + ".linalg")
 
-from .linalg import matrix_transpose, vecdot # noqa: F401
+__import__(__package__ + ".fft")
 
-__array_api_version__ = '2024.12'
+from ..common._helpers import *  # noqa: F403
+from .linalg import matrix_transpose, vecdot  # noqa: F401
+
+try:
+    # Used in asarray(). Not present in older versions.
+    from numpy import _CopyMode  # noqa: F401
+except ImportError:
+    pass
+
+__array_api_version__ = "2024.12"
