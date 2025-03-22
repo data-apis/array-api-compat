6a17007a32cf38f9328453b27ff6a59eeca85f53
jorenham <jhammudoglu@gmail.com>
2025-03-22 21:56:31 +0100

    TYP: fix (or ignore) typing errors in `common._helpers` (and curse at cupy)

--- array_api_compat/common/_typing.py
+++ array_api_compat/common/_typing.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
+
 from types import ModuleType as Namespace
-from typing import Any, TypeVar, Protocol
+from typing import Any, Protocol, TypeVar
 
 __all__ = [
     "Array",
+    "SupportsArrayNamespace",
     "DType",
     "Device",
+    "HasShape",
     "Namespace",
     "NestedSequence",
     "SupportsBufferProtocol",
@@ -16,6 +19,15 @@ _T_co = TypeVar("_T_co", covariant=True)
 class NestedSequence(Protocol[_T_co]):
     def __getitem__(self, key: int, /) -> _T_co | NestedSequence[_T_co]: ...
     def __len__(self, /) -> int: ...
+
+
+class SupportsArrayNamespace(Protocol[_T_co]):
+    def __array_namespace__(self, /, *, api_version: str | None) -> _T_co: ...
+
+
+class HasShape(Protocol[_T_co]):
+    @property
+    def shape(self, /) -> _T_co: ...
 
 
 SupportsBufferProtocol = Any
