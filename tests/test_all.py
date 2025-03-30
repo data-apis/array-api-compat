71d90ead399c03f5fcbc15d205d7cedb6bc9825c
Lucas Colley <lucas.colley8@gmail.com>
2025-03-30 09:19:56 +0100

    Update test_all.py
    Co-authored-by: Evgeni Burovski <evgeny.burovskiy@gmail.com>

--- tests/test_all.py
+++ tests/test_all.py
@@ -33,6 +33,7 @@ def test_all(library):
     else:
         import_(library, wrapper=True)
 
+    # NB: iterate over a copy to avoid a "dictionary size changed" error
     for mod_name in sys.modules.copy():
         if not mod_name.startswith('array_api_compat.' + library):
             continue
