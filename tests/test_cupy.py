7c3d68c47147663399cf4f23de24b9a4193d6f65
crusaderky <crusaderky@gmail.com>
2025-05-15 09:01:14 +0100

    TST: fix cupy `to_device` test on multiple devices

--- tests/test_cupy.py
+++ tests/test_cupy.py
@@ -8,15 +8,17 @@ from cupy.cuda import Stream
 def test_to_device_with_stream():
     devices = xp.__array_namespace_info__().devices()
     streams = [
-        Stream(),
-        Stream(non_blocking=True), 
-        Stream(null=True),
-        Stream(ptds=True), 
-        123,  # dlpack stream
+        lambda: Stream(),
+        lambda: Stream(non_blocking=True), 
+        lambda: Stream(null=True),
+        lambda: Stream(ptds=True), 
+        lambda: 123,  # dlpack stream
     ]
 
     a = xp.asarray([1, 2, 3])
     for dev in devices:
-        for stream in streams:
+        for stream_gen in streams:
+            with dev:
+                stream = stream_gen()
             b = to_device(a, dev, stream=stream)
             assert device(b) == dev
